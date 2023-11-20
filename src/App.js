d855534249917825650337b270e37df9aa14b26f
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-11-20 12:42:56 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import React from "react";
-import Navbar from "./components/navbar/Navbar";
-import Home from "./pages/home/Home";
-import About from "./pages/about/About";
-import TermsAndConditions from "./pages/terms/TermsAndConditions";
-import Contact from "./pages/contact/Contact";
-import Projects from "./pages/projects/Projects";
-import { Routes, Route } from "react-router-dom";
-import Footer from "./components/footer/Footer";
+import React from 'react'
+import Navbar from "./components/navbar/Navbar"
+import Home from './pages/home/Home'
+import Projects from "./pages/projects/Projects"
+import { Routes, Route } from "react-router-dom"
+import Footer from './components/footer/Footer'
+import About from './pages/about/About'
+import TermsAndConditions from './pages/terms/TermsAndConditions'
+import Contact from './pages/contact/Contact'
 
 const App = () => {
   return (
     <>
-      <Navbar />
+    <Navbar/>
+    <Routes>
+      <Route path = "/" element = {<Home/>}/>
+      <Route path = "/projects" element = {<Projects/>}/>
+      <Route path = "/about-us" element = {<About/>}/>
+      <Route path = "/terms-and-conditions" element = {<TermsAndConditions/>}/>
+      <Route path = "/contact-us" element = {<Contact/>}/>
+    </Routes>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-
-      <Footer />
+    <Footer/>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
